82e2697ca41e0aecbf0f3a7f58cd576fdfd6d3b8
Yar <yarsher.dev@gmail.com>
2019-04-03 17:17:50 -0400

    bugFixes

--- app/src/main/java/com/yarsher/at/splashapp/Splash.java
+++ app/src/main/java/com/yarsher/at/splashapp/Splash.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
@@ -23,22 +24,34 @@ import java.net.URLConnection;
 
 public class Splash extends Activity {
 
+
     int i;
-    String getURL = "http://127.0.0.1:8980/myserver/bigfile.txt";
-    String ip204 = "http://127.0.0.1:8980/myserver/return204.php";
-    public static int connectTimeout = 10000;
-    public static int readTimeout = 10000;
+
+    String getURL = "http://www.your-server.com/english-proper-names.txt";
+    String ip204 =  "http://www.your-server.com/return204.php";
+
+    public static int ConnectTimeout = 10000;
+    public static int ReadTimeout = 10000;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_splash);
-        if (haveNetworkConnection()){
+
+        this.setContentView(R.layout.activity_splash);
+
+        // Setup the ActionBar and the Spinner in the ActionBar
+        getActionBar().setDisplayShowTitleEnabled(true);
+        getActionBar().setSubtitle("Practical Android");
+        getActionBar().setTitle("Splash");
+
+        if (haveNetworkConnection()) {
             new HttpDownload().execute();
-        }else {
+        }
+        else {
             noConnection();
         }
     }
@@ -61,31 +74,28 @@ public class Splash extends Activity {
         return HaveConnectedWifi || HaveConnectedMobile;
     }
 
-    public class HttpDownload extends AsyncTask<Void, String, Void>{
-
-
+    public class HttpDownload extends AsyncTask<Void, String, Void> {
         @Override
-        protected Void doInBackground(Void... voids) {
-            publishProgress("Connecting", "0");
+        protected Void doInBackground(Void... unused) {
+            publishProgress("Connecting","0");
             String fromServer = "";
             int BUFFER_SIZE = 2000;
             float fsize = 890000;
             InputStream in = null;
-
-            //check reachability
             try {
+                // Check for reachability
                 URL url = new URL(ip204);
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setConnectTimeout(connectTimeout);
-                conn.setReadTimeout(readTimeout);
+                conn.setConnectTimeout(ConnectTimeout);
+                conn.setReadTimeout(ReadTimeout);
                 conn.setRequestMethod("HEAD");
                 in = conn.getInputStream();
                 int status = conn.getResponseCode();
                 in.close();
                 conn.disconnect();
-                if (status == HttpURLConnection.HTTP_NO_CONTENT){
-                    //server is reachable => initiate the download
-                    publishProgress("Reachable", "0");
+                if (status == HttpURLConnection.HTTP_NO_CONTENT) {
+                    // Server is reachabile, so initiate the download
+                    publishProgress("Connecting:", "0");
                     in = OpenHttpConnection(getURL);
                     InputStreamReader isr = new InputStreamReader(in);
                     int charRead;
@@ -95,31 +105,32 @@ public class Splash extends Activity {
                         String readString = String.copyValueOf(inputBuffer, 0, charRead);
                         fromServer += readString;
                         inputBuffer = new char[BUFFER_SIZE];
-                    //---update the progress
+                        //---update the progress
                         float ratio = (fromServer.length() / fsize) * 100;
                         int num = (int) ratio;
-                        publishProgress("Connecting: " + String.valueOf(num) + "%",
-                                String.valueOf(num));
+                        publishProgress("Connecting: " + String.valueOf(num) + "%", String.valueOf(num));
                     }
                     in.close();
                 } else {
                     publishProgress("Not Reachable", "0");
                     failedReach();
                 }
-
-
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
             } catch (IOException e) {
-                e.printStackTrace();
                 failedDownload();
             }
-
             publishProgress("Completed","100");
-
-
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+
+            startActivity(new Intent(Splash.this, MainActivity.class));
+            finish();
+        }
+
+        @Override
+        protected void onPreExecute() {
         }
 
         @Override
@@ -131,31 +142,6 @@ public class Splash extends Activity {
             progressBar.setProgress(num);
         }
     }
-
-
-
-    public static InputStream OpenHttpConnection(String urlString) throws IOException {
-        InputStream in = null;
-        int response = -1;
-        URL url = new URL(urlString);
-        URLConnection conn = url.openConnection();
-        if (!(conn instanceof HttpURLConnection)) throw new IOException("Not an HTTP connection");
-        try {
-            HttpURLConnection httpConn = (HttpURLConnection) conn;
-            httpConn.setAllowUserInteraction(false);
-            httpConn.setInstanceFollowRedirects(true);
-            httpConn.setRequestMethod("GET");
-            httpConn.connect();
-            response = httpConn.getResponseCode();
-            if (response == HttpURLConnection.HTTP_OK) {
-                in = httpConn.getInputStream();
-            }
-        } catch (Exception ex) {
-            throw new IOException("Error connecting");
-        }
-        return in;
-    }
-
 
     public void noConnection() {
         AlertDialog alertDialog = new AlertDialog.Builder(this).create();
@@ -198,8 +184,32 @@ public class Splash extends Activity {
 
     }
 
+    public static InputStream OpenHttpConnection(String urlString) throws IOException {
+        InputStream in = null;
+        int response = -1;
 
+        URL url = new URL(urlString);
+        URLConnection conn = url.openConnection();
 
+        if (!(conn instanceof HttpURLConnection)) throw new IOException("Not an HTTP connection");
+
+        try{
+            HttpURLConnection httpConn = (HttpURLConnection) conn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+
+            response = httpConn.getResponseCode();
+            if (response == HttpURLConnection.HTTP_OK) {
+                in = httpConn.getInputStream();
+            }
+        }
+        catch (Exception ex) {
+            throw new IOException("Error connecting");
+        }
+        return in;
+    }
 
 
 }
