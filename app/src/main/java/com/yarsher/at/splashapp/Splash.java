19f4c1ca6412597f36794343d2f36baa6c38fd46
Yar <yarsher.dev@gmail.com>
2019-04-03 16:35:58 -0400

    onProgressUpdate

--- app/src/main/java/com/yarsher/at/splashapp/Splash.java
+++ app/src/main/java/com/yarsher/at/splashapp/Splash.java
@@ -5,7 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class Splash extends AppCompatActivity {
 
@@ -34,19 +39,94 @@ public class Splash extends AppCompatActivity {
 
         @Override
         protected Void doInBackground(Void... voids) {
-            publicProgress("Connecting", "0");
+            publishProgress("Connecting", "0");
             String fromServer = "";
             int BUFFER_SIZE = 2000;
             float fsize = 890000;
             InputStream in = null;
 
-            try{
+            //check reachability
+            try {
+                URL url = new URL(ip204);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setConnectTimeout(connectTimeout);
+                conn.setReadTimeout(readTimeout);
+                conn.setRequestMethod("HEAD");
+                in = conn.getInputStream();
+                int status = conn.getResponseCode();
+                in.close();
+                conn.disconnect();
+                if (status == HttpURLConnection.HTTP_NO_CONTENT){
+                    //server is reachable => initiate the download
+                    publishProgress("Reachable", 0);
+                    in = OppenHttpConnection(getURL);
+                    InputStream isr = new InputStream(in);
+                    int charRead;
+                    char[] inputBuffer = new char[BUFFER_SIZE];
+                    while ((charRead = isr.read(inputBuffer))>0) {
+                        //---convert the chars to a String---
+                        String readString = String.copyValueOf(inputBuffer, 0, charRead);
+                        fromServer += readString;
+                        inputBuffer = new char[BUFFER_SIZE];
+                    //---update the progress
+                        float ratio = (fromServer.length() / fsize) * 100;
+                        int num = (int) ratio;
+                        publishProgress("Connecting: " + String.valueOf(num) + "%",
+                                String.valueOf(num));
+                    }
+                    in.close();
+                } else {
+                    publishProgress("Not Reachable", "0");
+                    failedReach();
+                }
 
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+                failedDownload();
             }
+
+            publishProgress("Completed","100");
 
 
             return null;
         }
+
+        @Override
+        protected void onProgressUpdate(String... item) {
+            TextView txt = (TextView) findViewById(R.id.text);
+            txt.setText(item[0]);
+            ProgressBar progressBar = (ProgressBar) findViewById(R.id.progBar);
+            int num = Integer.parseInt(item[1]);
+            progressBar.setProgress(num);
+        }
+    }
+
+
+
+    public static InputStream OpenHttpConnection(String urlString) throws IOException {
+        InputStream in = null;
+        int response = -1;
+        URL url = new URL(urlString);
+        URLConnection conn = url.openConnection();
+        if (!(conn instanceof HttpURLConnection)) throw new IOException("Not an HTTP connection");
+        try {
+            HttpURLConnection httpConn = (HttpURLConnection) conn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+            response = httpConn.getResponseCode();
+            if (response == HttpURLConnection.HTTP_OK) {
+                in = httpConn.getInputStream();
+            }
+        } catch (Exception ex) {
+            throw new IOException("Error connecting");
+        }
+        return in;
     }
 
 
